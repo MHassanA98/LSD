7742481f39c237b5f1a315b1ef450ef4b68a0894
Hassan <mhassana@live.com>
2020-05-07 19:51:55 +0500

    shutdown service

--- routes/ADrawer.js
+++ routes/ADrawer.js
@@ -15,6 +15,34 @@ export default function CustomerDrawer({navigation}) {
         setScreen(route)
     })
 
+    function onShutdown(){
+
+        Alert.alert(
+            "Shutdown Deliver",
+            "Are you sure you want to suspend the delivery service?",
+            [
+              {
+                text: "Yes",
+                // onPress: ()=>{
+                //   firebase.auth().signOut()
+                //   .then(() => {
+                //     navigation.navigate("LoginStack")
+                //   })
+                //   .catch(function(error) {
+                //     alert(error)
+                //   })
+                // }
+              },
+              {
+                text: "No",
+  
+              }
+            ]
+            , {cancelable: false}
+          )
+
+    }
+
     function onSignoutPress() {
 
         Alert.alert(
@@ -83,7 +111,7 @@ export default function CustomerDrawer({navigation}) {
 
             <View style={styles.lineStyle}/>
 
-            <TouchableOpacity style={[styles.TouchOpp,{}]} onPress={()=>{}} activeOpacity={0.3}>
+            <TouchableOpacity style={[styles.TouchOpp,{}]} onPress={()=>{onShutdown}} activeOpacity={0.3}>
                 <MIcon style={styles.FAIcon} name='power-settings-new' size={24}/>
                 <Text style={styles.TextDraw}>
                     Shutdown
